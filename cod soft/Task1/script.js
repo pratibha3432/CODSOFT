ef8f4ff50b188eb7bb693a96e6ee6ef541d82ab4
pratibha3432 <pratibhakesarkar48@gmail.com>
2024-06-09 23:13:31 +0530

    Add files via upload

---
+++ cod soft/Task1/script.js
@@ -0,0 +1,9 @@
+function about() {
+    document.getElementById("click").style.display = "block";
+}
+function course() {
+    document.getElementById("course").style.display = "block";
+}
+function apply() {
+    document.getElementById("apply").style.display = "block";
+}
